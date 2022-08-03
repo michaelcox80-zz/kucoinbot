1f37f5fc243c9b92458d760c139d8637691bba58
michael cox <michaelcox80@gmail.com>
2022-08-03 08:23:57 +0300

    removed

--- bin/both_not_periodic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# source FULL_PATH_TO_VIRTUALENV
-# example : 
-source /home/pmpls13/python-virtual-environments/kucoin/bin/activate
-# Leave line commented if not using virtualenv
-
-python3 ../kucoin/kucoin/runner.py "false" &
-python3 ../gateio/gateio/runner.py "false"
